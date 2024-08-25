4f18c27b8803b984d308ff9d295b05471e2ce495
muthumanikandan390d <muthumanikandan390d@gmail.com>
2024-08-25 16:16:54 +0530

    nav-bar

--- FRONTEND/ecomfrontend/src/App.jsx
+++ FRONTEND/ecomfrontend/src/App.jsx
@@ -2,10 +2,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home-folder/Home";
 import Login from "./pages/Login-folder/Login";
+import NavBar from "./components/navbar-folder/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
     <Routes>
     <Route index element={<Home />} />
     <Route path="login" element={<Login/>}/>
--- FRONTEND/ecomfrontend/src/components/navbar-folder/NavBar.jsx
+++ FRONTEND/ecomfrontend/src/components/navbar-folder/NavBar.jsx
@@ -1,15 +1,28 @@
-
+import {assets} from '../../assets/assets'
+import styles from './navBar.module.css'
 
 function NavBar() {
   return (
-    <header>
+    <header >
       <nav>
-        <img ></img>
-        <ul>
-          <li>
-
-          </li>
+        <div className={styles.navContainer}>
+        <div className={styles.imgContainer}>
+        <img className={styles.navImage} src={assets.logo}></img>
+        </div>
+        <div className={styles.navLinkContainer} >
+        <ul className={styles.navUl}>
+          <a href='#'>
+            <h1> about </h1>
+          </a>
+          <a href='#'>
+            <h1> contact </h1>
+          </a>
+          <a href='#'>
+            <h1> cell no </h1>
+          </a>
         </ul>
+        </div>
+        </div>
       </nav>
     </header>
   )
